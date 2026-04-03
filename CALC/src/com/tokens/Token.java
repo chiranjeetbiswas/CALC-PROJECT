11cd559beb7c71c43208f76370116d2562552926
unknown <ajeetyadav946oy883744@gmail.com>
2026-04-03 21:26:49 +0530

    Tokens List Creation Done

--- CALC/src/com/tokens/Token.java
+++ CALC/src/com/tokens/Token.java
@@ -1,5 +1,33 @@
 package com.tokens;
 
 public class Token {
-
+  private final TokenType type;
+	private final String value;
+	private final int line;
+	
+	public Token(TokenType type, String value, int line) {
+		this.type = type;
+		this.value = value;
+		this.line = line;
+	}
+	
+	public TokenType getType() {
+		return this.type;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+	
+	public int getLine() {
+		return this.line;
+	}
+	
+	@Override
+	public String toString() {
+		if(this.getValue() == "\n") {
+			return this.getType() + ": " + "\\n";
+		}
+		return this.getType() + ": " + this.getValue();
+	}
 }
--- CALC/src/com/tokens/Tokenizer.java
+++ CALC/src/com/tokens/Tokenizer.java
@@ -1,5 +1,174 @@
 package com.tokens;
 
-public class Tokenizer {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tokenizer {
+  public String source;
+	public int cur_pos = 0;
+	
+	public Tokenizer(String source) {
+		this.source = source;
+	}
+	
+	public List<Token> tokenize(){
+		List<Token> token = new ArrayList<>();
+		int line = 1;
+		int i = cur_pos;
+		
+		//indent
+		//..
+		
+		while(i < source.length()) {
+			char c = source.charAt(i);
+			if(c == '\r') {
+				i++;
+				continue;
+			}
+			
+			if(c == ' ' || c == '\t') {
+				i++;
+				continue;
+			}
+			
+			else if(c == '\n') {
+				token.add(new Token(TokenType.NEWLINE,"\n",line));
+				line++;
+				i++;
+			}
+			
+			else if(Character.isLetter(c) || c == '_') {
+				StringBuilder ID = new StringBuilder();
+				while(i<source.length() && (Character.isLetterOrDigit(source.charAt(i)) || source.charAt(i) == '_')) {
+					ID.append(source.charAt(i));
+					i++;
+				}
+				
+				token.add(new Token(TokenType.IDENTIFIER,ID.toString(),line));
+			}
+			
+			else if(Character.isDigit(c)) {
+				int count = 0;
+				StringBuilder NUM = new StringBuilder();
+				while(i<source.length() && (Character.isDigit(source.charAt(i)) || source.charAt(i) == '.')) {
+					c = source.charAt(i);
+					if(c == '.') {
+						count++;
+						//Throw Exception
+						if(count>1) {
+							throw new RuntimeException("Invalid number format at line "+ line);
+						}
+					}
+					NUM.append(c);
+					i++;
+				}
+				if(!NUM.isEmpty() && NUM.charAt(0)!='.' && NUM.charAt(NUM.length() - 1)!='.') {
+					token.add(new Token(TokenType.NUMBER, NUM.toString(), line));
+				}
+				else {
+					throw new RuntimeException("Invalid number format at line "+ line);
+				}
+				
+			}
+			
+			else if(c == '"') {
+				i++;
+				StringBuilder STR = new StringBuilder();
+				while(i<source.length()) {
+					c = source.charAt(i);
+					
+					if(c == '"') {
+						break;
+					}
+					STR.append(c);
+					i++;
+				}
+				if(i <source.length() && source.charAt(i) == '"') {
+					token.add(new Token(TokenType.STRING, STR.toString(), line));
+					i++;
+				}else {
+					throw new RuntimeException("Unterminated string at line "+ line);
+				}
+			}
+			
+			else if(i<source.length() - 1 && c == ':' && source.charAt(i+1) == '=') {
+				token.add(new Token(TokenType.ASSIGN,":=",line));
+				i += 2;
+			}
+			
+			else if(i<source.length() - 1 && c == '>' && source.charAt(i+1) == '>') {
+				token.add(new Token(TokenType.PRINT, ">>",line));
+				i += 2;
+			}
+			
+			else if(i<source.length() - 1 && c == '=' && source.charAt(i+1) == '>') {
+				token.add(new Token(TokenType.ARROW, "=>", line));
+				i += 2;
+			}
+			
+			else if(i<source.length() - 1 && c == '=' && source.charAt(i+1) == '=') {
+				token.add(new Token(TokenType.DEQUAL, "==", line));
+				i += 2;
+			}
+			
+			else if(c == '?') {
+				token.add(new Token(TokenType.IF,"?",line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '@') {
+				token.add(new Token(TokenType.LOOP,"@",line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '+') {
+				token.add(new Token(TokenType.ADD, "+", line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '-') {
+				token.add(new Token(TokenType.SUBTRACT,"-",line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '*') {
+				token.add(new Token(TokenType.MULTIPLY, "*", line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '/') {
+				token.add(new Token(TokenType.DIVIDE,"/", line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '>') {
+				token.add(new Token(TokenType.GREATER, ">",line));
+				i++;
+				continue;
+			}
+			
+			else if(c == '<') {
+				token.add(new Token(TokenType.LESS,"<",line));
+				i++;
+				continue;
+			}
+			//Throw Exception
+			else {
+				i++;
+				throw new RuntimeException("Unexpected character: "+c+" at line " + line);
+			}
+		}
+		token.add(new Token(TokenType.EOF, "EOF", line));
+		return token;
+  }
+  public static void main(String[] args) {
+		Tokenizer t = new Tokenizer("x := 10\n>>\"hello\"\n? x > 5 =>\n@ 3 =>");
+		System.out.println(t.tokenize());
+	}
 }
